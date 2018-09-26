e2516df4c7fd6160c36cab848198ee8848b12813
Ema Pajić <ema.p25@gmail.com>
2018-09-26 12:40:40 +0200

    Update navigation.py
    Added functions for safety check, linear and angular moving

--- navigation.py
+++ navigation.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep 22 16:50:27 2018
-
-@author: user
+@author: EmaPajic
 """
 
 import rospy
@@ -14,33 +13,44 @@ from sensor_msgs.msg import LaserScan
 class Navigator:
     def __init__(self):
         self.scan_msg = None
+        rospy.init_node('navigation', anonymous=True)
+        self.rate = rospy.Rate(25) # 25hz
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size = 10)
+        self.safety_vel = rospy.Publisher('cmd_vel_mux/input/safety_controller', Twist, queue_size = 1)
+        rospy.Subscriber('scan', LaserScan, self.callback) 
+        
     def callback(self, msg):
         self.scan_msg = msg
-    def navigation(self):
-        cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size = 10)
-        safety_vel = rospy.Publisher('cmd_vel_mux/input/safety_controller', Twist, queue_size = 1)
-        rospy.init_node('navigation', anonymous=True)
-        rospy.Subscriber('scan', LaserScan, self.callback)
-        rate = rospy.Rate(25) # 25hz
-        while not rospy.is_shutdown():
-            if self.scan_msg is not None:
+        
+    def safety_check(self):
+        if self.scan_msg is not None:
                 for r in self.scan_msg.ranges:
                     if r > self.scan_msg.range_min and r < self.scan_msg.range_max:
                         if r < max(self.scan_msg.range_min + 0.3, 0.3):
-                            turn_cmd = Twist()
-                            turn_cmd.angular.z = np.radians(180)
-                            safety_vel.publish(turn_cmd)
-                            rate.sleep()
+                            self.move_angular_cmd(True,179,180,1)
                             break
-            move_cmd = Twist()
-            move_cmd.linear.x = np.random.uniform(0,1)
-            cmd_vel.publish(move_cmd)
-            rate.sleep()
-            if np.random.uniform(0,1) < 0.4: 
-                turn_cmd = Twist()
-                turn_cmd.angular.z = np.radians(np.random.uniform(-360,360))
-                cmd_vel.publish(turn_cmd)
-                rate.sleep()
+                        
+    def move_linear_cmd(self, min_speed = 0, max_speed = 1):
+        move_cmd = Twist()
+        move_cmd.linear.x = np.random.uniform(min_speed,max_speed)
+        self.cmd_vel.publish(move_cmd)
+        self.rate.sleep()
+        
+    def move_angular_cmd(self, safety, min_angle = -360, max_angle = 360, probability = 1):
+        if np.random.uniform(0,1) < probability: 
+            turn_cmd = Twist()
+            turn_cmd.angular.z = np.radians(np.random.uniform(min_angle,max_angle))
+            if safety == True:
+                self.safety_vel.publish(turn_cmd)
+            else:
+                self.cmd_vel.publish(turn_cmd)
+            self.rate.sleep()
+        
+    def navigation(self):
+        while not rospy.is_shutdown():
+            self.safety_check()
+            self.move_linear_cmd(0,1)
+            self.move_angular_cmd(False,-360,360,0.4)
         
 if __name__ == '__main__':
     try:
